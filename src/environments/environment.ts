a40ad72d74d2a7255d3951d43083abcce3d4936f
Guilherme Pereira <guilherme.pereira@insidesistemas.com.br>
2026-03-26 10:41:19 -0300

    feat: melhorias pagina de jogos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://pig-league-api.onrender.com',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://pig-league-api.onrender.com',
+  // apiUrl: 'http://localhost:3000',
 };
